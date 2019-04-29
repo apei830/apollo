b9c8418f57ce03c476a3f7ccc6222f22aacee363
apei830 <apei830@qq.com>
2019-04-29 17:29:34 +0800

    添加 uat, prod, dev 等

--- scripts/build.bat
+++ scripts/build.bat
@@ -11,7 +11,7 @@ set apollo_portal_db_username="root"
 set apollo_portal_db_password="derbysoft"
 
 rem meta server url, different environments should have different meta server addresses
-set dev_meta="http://localhost:8080"
+set dev_meta="http://10.110.233.102/apollo"
 set fat_meta="http://someIp:8080"
 set uat_meta="http://10.110.233.102/apollo"
 set pro_meta="http://yetAnotherIp:8080"
@@ -27,14 +27,14 @@ cd ..
 rem package config-service
 echo "==== starting to build config-service ===="
 
-call mvn clean package -DskipTests -pl apollo-configservice -am -Dapollo_profile=github -Dspring_datasource_url=%apollo_config_db_url% -Dspring_datasource_username=%apollo_config_db_username% -Dspring_datasource_password=%apollo_config_db_password%
+rem call mvn clean package -DskipTests -pl apollo-configservice -am -Dapollo_profile=github -Dspring_datasource_url=%apollo_config_db_url% -Dspring_datasource_username=%apollo_config_db_username% -Dspring_datasource_password=%apollo_config_db_password%
 
 echo "==== building config-service  finished ===="
 
 rem package  admin-service
 echo "==== starting to build admin-service ===="
 
-call mvn clean package -DskipTests -pl apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=%apollo_config_db_url% -Dspring_datasource_username=%apollo_config_db_username% -Dspring_datasource_password=%apollo_config_db_password%
+rem call mvn clean package -DskipTests -pl apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=%apollo_config_db_url% -Dspring_datasource_username=%apollo_config_db_username% -Dspring_datasource_password=%apollo_config_db_password%
 
 echo "==== building admin-service finished ===="
 
